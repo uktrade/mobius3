ecd44686a7ddc7cc3994e4e74de98a2fee3de6b1
Michal Charemza <michal@charemza.name>
2019-07-24 15:43:25 +0100

    (tests) Multiple nested renames

--- test.py
+++ test.py
@@ -490,6 +490,47 @@ class TestIntegration(unittest.TestCase):
             b'some-bytes')
 
     @async_test
+    async def test_file_in_renamed_twice_nested_directory_after_delay(self):
+        delete_dir = create_directory('/s3-home-folder')
+        self.add_async_cleanup(delete_dir)
+
+        start, stop = syncer_for('/s3-home-folder')
+        self.add_async_cleanup(stop)
+        await start()
+
+        dirname_1 = str(uuid.uuid4())
+        dirname_2 = str(uuid.uuid4())
+        dirname_3 = str(uuid.uuid4())
+        dirname_4 = str(uuid.uuid4())
+
+        os.mkdir(f'/s3-home-folder/{dirname_1}')
+        os.mkdir(f'/s3-home-folder/{dirname_1}/{dirname_2}')
+        filename = str(uuid.uuid4())
+
+        with open(f'/s3-home-folder/{dirname_1}/{dirname_2}/{filename}', 'wb') as file:
+            file.write(b'some-bytes')
+
+        await await_upload()
+
+        os.rename(f'/s3-home-folder/{dirname_1}', f'/s3-home-folder/{dirname_3}')
+        os.rename(f'/s3-home-folder/{dirname_3}', f'/s3-home-folder/{dirname_4}')
+
+        await await_upload()
+
+        request, close = get_docker_link_and_minio_compatible_http_pool()
+        self.add_async_cleanup(close)
+
+        self.assertEqual(
+            await object_code(request, f'{dirname_1}/{dirname_2}/{filename}'),
+            b'404')
+        self.assertEqual(
+            await object_code(request, f'{dirname_3}/{dirname_2}/{filename}'),
+            b'404')
+        self.assertEqual(
+            await object_body(request, f'{dirname_4}/{dirname_2}/{filename}'),
+            b'some-bytes')
+
+    @async_test
     async def test_file_rename_immediate(self):
         delete_dir = create_directory('/s3-home-folder')
         self.add_async_cleanup(delete_dir)
