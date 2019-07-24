9c598330d7749a9ba6a30f522a9dc8bb17f991ba
Michal Charemza <michal@charemza.name>
2019-07-24 14:10:15 +0100

    (build) Collect code coverage in CircleCI

---
+++ test_with_coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Ensure we gather coverage from all processes
+SITE_PACKAGES_DIR=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+echo "import coverage; coverage.process_startup()" > "${SITE_PACKAGES_DIR}/coverage.pth"
+export COVERAGE_PROCESS_START=.coveragerc
+
+python3 setup.py test "$@"
+
+coverage combine
+coverage xml
