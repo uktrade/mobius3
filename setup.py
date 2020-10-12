8b9741dde9daa6160fa2f25b0916e36971450b9e
Michal Charemza <michal@charemza.name>
2020-10-12 12:55:13 +0100

    build(release): v0.0.34

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='mobius3',
-    version='0.0.33',
+    version='0.0.34',
     author='Department for International Trade',
     author_email='webops@digital.trade.gov.uk',
     description='Continuously and asynchronously sync a local folder to an S3 bucket',
