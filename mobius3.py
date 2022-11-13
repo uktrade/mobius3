1ab38c040bc736f2be192f2bc86fabb1f3ca0c0a
Michal Charemza <michal@charemza.name>
2022-11-13 13:03:18 +0000

    refactor: move function that doesn't need to be on global scope inside closure
    DT-340

--- mobius3.py
+++ mobius3.py
@@ -186,63 +186,62 @@ def Pool(
     )
 
 
-def aws_sigv4_headers(access_key_id, secret_access_key, pre_auth_headers,
-                      service, region, host, method, path, params, content_hash):
-    algorithm = 'AWS4-HMAC-SHA256'
-
-    now = datetime.datetime.utcnow()
-    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = now.strftime('%Y%m%d')
-    credential_scope = f'{datestamp}/{region}/{service}/aws4_request'
-
-    pre_auth_headers_lower = tuple(
-        (header_key.lower(), ' '.join(header_value.split()))
-        for header_key, header_value in pre_auth_headers
-    )
-    required_headers = (
-        ('host', host),
-        ('x-amz-content-sha256', content_hash),
-        ('x-amz-date', amzdate),
-    )
-    headers = sorted(pre_auth_headers_lower + required_headers)
-    signed_headers = ';'.join(key for key, _ in headers)
-
-    def signature():
-        def canonical_request():
-            canonical_uri = urllib.parse.quote(path, safe='/~')
-            quoted_params = sorted(
-                (urllib.parse.quote(key, safe='~'), urllib.parse.quote(value, safe='~'))
-                for key, value in params
-            )
-            canonical_querystring = '&'.join(f'{key}={value}' for key, value in quoted_params)
-            canonical_headers = ''.join(f'{key}:{value}\n' for key, value in headers)
-
-            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
-                   f'{canonical_headers}\n{signed_headers}\n{content_hash}'
-
-        def sign(key, msg):
-            return hmac.new(key, msg.encode('ascii'), hashlib.sha256).digest()
-
-        string_to_sign = f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
-                         hashlib.sha256(canonical_request().encode('ascii')).hexdigest()
-
-        date_key = sign(('AWS4' + secret_access_key).encode('ascii'), datestamp)
-        region_key = sign(date_key, region)
-        service_key = sign(region_key, service)
-        request_key = sign(service_key, 'aws4_request')
-        return sign(request_key, string_to_sign).hex()
-
-    return (
-        ('authorization', (
-            f'{algorithm} Credential={access_key_id}/{credential_scope}, '
-            f'SignedHeaders={signed_headers}, Signature=' + signature())
-         ),
-        ('x-amz-date', amzdate),
-        ('x-amz-content-sha256', content_hash),
-    ) + pre_auth_headers
-
-
 def AWSAuth(service, region, client, get_credentials, content_hash=hashlib.sha256().hexdigest()):
+
+    def aws_sigv4_headers(access_key_id, secret_access_key, pre_auth_headers,
+                          service, region, host, method, path, params, content_hash):
+        algorithm = 'AWS4-HMAC-SHA256'
+
+        now = datetime.datetime.utcnow()
+        amzdate = now.strftime('%Y%m%dT%H%M%SZ')
+        datestamp = now.strftime('%Y%m%d')
+        credential_scope = f'{datestamp}/{region}/{service}/aws4_request'
+
+        pre_auth_headers_lower = tuple(
+            (header_key.lower(), ' '.join(header_value.split()))
+            for header_key, header_value in pre_auth_headers
+        )
+        required_headers = (
+            ('host', host),
+            ('x-amz-content-sha256', content_hash),
+            ('x-amz-date', amzdate),
+        )
+        headers = sorted(pre_auth_headers_lower + required_headers)
+        signed_headers = ';'.join(key for key, _ in headers)
+
+        def signature():
+            def canonical_request():
+                canonical_uri = urllib.parse.quote(path, safe='/~')
+                quoted_params = sorted(
+                    (urllib.parse.quote(key, safe='~'), urllib.parse.quote(value, safe='~'))
+                    for key, value in params
+                )
+                canonical_querystring = '&'.join(f'{key}={value}' for key, value in quoted_params)
+                canonical_headers = ''.join(f'{key}:{value}\n' for key, value in headers)
+
+                return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
+                       f'{canonical_headers}\n{signed_headers}\n{content_hash}'
+
+            def sign(key, msg):
+                return hmac.new(key, msg.encode('ascii'), hashlib.sha256).digest()
+
+            string_to_sign = f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
+                             hashlib.sha256(canonical_request().encode('ascii')).hexdigest()
+
+            date_key = sign(('AWS4' + secret_access_key).encode('ascii'), datestamp)
+            region_key = sign(date_key, region)
+            service_key = sign(region_key, service)
+            request_key = sign(service_key, 'aws4_request')
+            return sign(request_key, string_to_sign).hex()
+
+        return (
+            ('authorization', (
+                f'{algorithm} Credential={access_key_id}/{credential_scope}, '
+                f'SignedHeaders={signed_headers}, Signature=' + signature())
+             ),
+            ('x-amz-date', amzdate),
+            ('x-amz-content-sha256', content_hash),
+        ) + pre_auth_headers
 
     class _AWSAuth(httpx.Auth):
         async def async_auth_flow(self, request):
